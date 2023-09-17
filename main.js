707810d30f05668223a368864acaa0ad6de0d51c
Ari Denary <aridenary@gmail.com>
2023-09-17 08:47:00 -0400

    feat: update stats with town name

--- main.js
+++ main.js
@@ -79,7 +79,7 @@ Papa.parse('./data/crashes.csv', {
 
         let tsCoef = 100000.0 // original timestamp needs to be multiplied by this to work in JS
 
-        let updateStatsText = function(
+        function updateStatsText(
           formattedFrom,
           formattedTo,
           crashesTotal,
@@ -87,7 +87,8 @@ Papa.parse('./data/crashes.csv', {
           crashesCyc,
           crashesHitAndRun,
           crashesFatal,
-          filtered
+          filtered,
+          townName
         ) {
 
           let text = formattedFrom === formattedTo
@@ -96,7 +97,7 @@ Papa.parse('./data/crashes.csv', {
               + formattedTo + "</span>")
 
           text += ', there ' + (crashesTotal === 1 ? 'was ' : 'were  <span class="orange fw5">') + (crashesTotal === 0 ? 'no' : crashesTotal.toLocaleString())
-          text += ' dangerous motor vehicle crash' + (crashesTotal === 1 ? '' : 'es') + '</span> in <b>Northeastern Connecticut</b>.'
+          text += ` dangerous motor vehicle crash${(crashesTotal === 1 ? '' : 'es')}</span> in <b> ${townName}</b>.`
 
           if (crashesTotal > 1) {
               text += ' Of those, <span class="dark-pink fw5">' + (crashesPed > 0 ? crashesPed.toLocaleString() : ' none');
@@ -119,144 +120,146 @@ Papa.parse('./data/crashes.csv', {
         // Given `from` and `to` timestamps, updates the heatmap layer.
         function updateHeatLayer (from, to) {
 
-            from = dateToTS(new Date(from * 1).setHours(0, 0, 0, 0)) / tsCoef;
-            to = dateToTS(new Date(to * 1).setHours(23, 59, 59, 0)) / tsCoef;
+          from = dateToTS(new Date(from * 1).setHours(0, 0, 0, 0)) / tsCoef;
+          to = dateToTS(new Date(to * 1).setHours(23, 59, 59, 0)) / tsCoef;
 
-            // All crashes between set dates
-            // Crashes variable assigned here from parse output of crashes.csv
-            let crashes = data.filter(function (point) {
-                return point.d >= from && point.d <= to;
-            })
+          // All crashes between set dates
+          // Crashes variable assigned here from parse output of crashes.csv
+          let crashes = data.filter(function (point) {
+              return point.d >= from && point.d <= to;
+          })
 
-            // Filter crashes based on checkboxes
-            function filterCrashes() {
-              return crashes.filter(function(point) {
-                return (( $('#local').prop('checked') ? point.r !== 1 : false)
-                || ( $('#highways').prop('checked') ? point.r === 1 : false))
+          // Filter crashes based on checkboxes
+          function filterCrashes() {
+            return crashes.filter(function(point) {
+              return (( $('#local').prop('checked') ? point.r !== 1 : false)
+              || ( $('#highways').prop('checked') ? point.r === 1 : false))
 
-                && (( $('#vehiclesOnly').prop('checked') ? (point.c === 0 && point.p === 0) : false)
-                || ( $('#cyclists').prop('checked') ? point.c === 1 : false)
-                || ( $('#pedestrians').prop('checked') ? point.p === 1 : false))
+              && (( $('#vehiclesOnly').prop('checked') ? (point.c === 0 && point.p === 0) : false)
+              || ( $('#cyclists').prop('checked') ? point.c === 1 : false)
+              || ( $('#pedestrians').prop('checked') ? point.p === 1 : false))
 
-                && (( $('#injury').prop('checked') ? point.s === 'A' : false)
-                || ( $('#fatal').prop('checked') ? point.s === 'K' : true))
+              && (( $('#injury').prop('checked') ? point.s === 'A' : false)
+              || ( $('#fatal').prop('checked') ? point.s === 'K' : true))
 
-                && (( $('#bikelane').prop('checked') ? point.blp === 'True' : true)
-                || ( !$('#bikelane').prop('checked') ? point.blp !== 'True' : false))
+              && (( $('#bikelane').prop('checked') ? point.blp === 'True' : true)
+              || ( !$('#bikelane').prop('checked') ? point.blp !== 'True' : false))
 
-                && (( $('#crashesHitAndRun').prop('checked') ? point.hr === 'True' : true)
-                || ( !$('#crashesHitAndRun').prop('checked') ? point.hr !== 'True' : false))
+              && (( $('#crashesHitAndRun').prop('checked') ? point.hr === 'True' : true)
+              || ( !$('#crashesHitAndRun').prop('checked') ? point.hr !== 'True' : false))
 
-                // filter point 'tn' for town name, or show all towns if empty string
-                && (( $('#town-name').val() === "")
-                || ( $('#town-name').val() === point.tn) ? true : false)
+              // filter point 'tn' for town name, or show all towns if empty string
+              && (( $('#town-name').val() === "")
+              || ( $('#town-name').val() === point.tn) ? true : false)
             });
           }
 
           let crashesFiltered = filterCrashes();
-            console.log("Crashes filtered = ", crashesFiltered);
 
-            updateStatsText(
-              tsToDate(from * 100000),  // Date from
-              tsToDate(to * 100000),  // Date to
-              crashes.length, // Total crashes
-              crashes.filter(function(p) {return p.p === 1}).length,  // Ped crashes
-              crashes.filter(function(p) {return p.c === 1}).length,  // Cyc crashes
-              crashes.filter(function(p) {return p.v === 'True'}).length, // Hit and run status
-              crashes.filter(function(p) {return p.s === 'K'}).length, // Fatal crashes
-              crashesFiltered.length
-            )
+          const area = $('#town-name').val() === "" ? 'Northeastern CT' : $('#town-name').val();
 
-            // Despite zoom, clear individual points
-            individualPoints.clearLayers();
+          updateStatsText(
+            tsToDate(from * 100000),  // Date from
+            tsToDate(to * 100000),  // Date to
+            crashes.length, // Total crashes
+            crashes.filter(function(p) {return p.p === 1}).length,  // Ped crashes
+            crashes.filter(function(p) {return p.c === 1}).length,  // Cyc crashes
+            crashes.filter(function(p) {return p.v === 'True'}).length, // Hit and run status
+            crashes.filter(function(p) {return p.s === 'K'}).length, // Fatal crashes
+            crashesFiltered.length,
+            area
+          )
 
-            // Update the heatlayer
-            let intensity = 20;
+          // Despite zoom, clear individual points
+          individualPoints.clearLayers();
 
-            // Main heatmap
-            document.addEventListener('DOMContentLoaded', function() {
-                // setLatLngs is a Leaflet function that accepts the filtered crashes and sets the heatmap layer to those crashes
-                heat.setLatLngs(
-                    crashesFiltered.map(function(point) { // crashesFiltered is the array of crashes that satisfy the filter criteria
-                    return [point.x, point.y, intensity]; // x and y are the lat and long of the crash, intensity is the intensity of the heatmap
+          // Update the heatlayer
+          let intensity = 20;
 
-                })
-            )
+          // Main heatmap
+          document.addEventListener('DOMContentLoaded', function() {
+              // setLatLngs is a Leaflet function that accepts the filtered crashes and sets the heatmap layer to those crashes
+              heat.setLatLngs(
+                  crashesFiltered.map(function(point) { // crashesFiltered is the array of crashes that satisfy the filter criteria
+                  return [point.x, point.y, intensity]; // x and y are the lat and long of the crash, intensity is the intensity of the heatmap
+
+              })
+          )
+          });
+
+
+          // If zoomed in all the way, show points instead of a heatmap
+          if ( map.getZoom() >= 12 ) {
+
+            heat.redraw();
+            let intensity = 1; // quickly adjusts intensity of heatmap
+
+            crashesFiltered.map(function(crash) { // crash function declared here
+
+            let diagramUrl = 'https://www.ctcrash.uconn.edu/MMUCCDiagram?id=' + crash.id + '&asImage=true'
+
+            // L.circleMarker is a Leaflet function that creates a circle marker at the lat and long of the crash
+            let circle = L.circleMarker([crash.x, crash.y], {
+                radius: 5,
+                color: '#000000',
+                fillColor: '#FFFFFF',
+                fillOpacity: 1,
+                opacity: 1,
+                weight: 2,
+                }).bindPopup(
+                    '<span class="avenir fw5"><p>Crash ID: <b>' + crash.id + '</b></p><p>'
+                    + tsToDate(crash.d * tsCoef) + ' at ' + crash.t + '</p>'
+                    + '<p>Severity: ' + (crash.s === 'K' ? 'Fatal crash' : crash.s === 'A' ? 'Suspected Serious Injury' : 'Property damage only'
+                    + '<br><p>Trafficway Ownership: ' + crash.s ==='' ? 'Public road' : 'Other')
+                    + '<br><p>Motor vehicle was driving on: ' + crash.o + (crash.h === null ? '' : ' and the nearest cross-street is ' + crash.h + '</p>')
+                    + '<p>There was ' + (crash.f === 'True' ? 'a bike lane ' : 'no bike lane ') + 'present.</p>'
+                    + '<a href="' + diagramUrl + '" target="_blank"><img src="' + diagramUrl + '" style="display:none" alt="Crash diagram" />Show crash diagram.</a>'
+                    + '</span><br>',
+                    { minWidth: 200 }
+                )
+
+            // Unused function, but could be used to show crash diagram
+            function showDiagram() {
+                var diagramElement = document.getElementById("diagram");
+                diagramElement.style.display = "block"
+            }
+
+            circle.on('popupopen', function () {
+                filters.style.display = "none";
+                map.setLatLng(map.getCenter() + [0, 0.0001]);
+                console.log(map.getCenter())
             });
 
+            circle.on('popupclose', function () {
+                filters.style.display = "block";
+            });
+
+            individualPoints.addLayer(circle); // add the circle to the layergroup
+            })
 
             // If zoomed in all the way, show points instead of a heatmap
-            if ( map.getZoom() >= 12 ) {
-
-                heat.redraw();
-                let intensity = 1; // quickly adjusts intensity of heatmap
-
-                crashesFiltered.map(function(crash) { // crash function declared here
-
-                let diagramUrl = 'https://www.ctcrash.uconn.edu/MMUCCDiagram?id=' + crash.id + '&asImage=true'
-
-                // L.circleMarker is a Leaflet function that creates a circle marker at the lat and long of the crash
-                let circle = L.circleMarker([crash.x, crash.y], {
-                    radius: 5,
-                    color: '#000000',
-                    fillColor: '#FFFFFF',
-                    fillOpacity: 1,
-                    opacity: 1,
-                    weight: 2,
-                    }).bindPopup(
-                        '<span class="avenir fw5"><p>Crash ID: <b>' + crash.id + '</b></p><p>'
-                        + tsToDate(crash.d * tsCoef) + ' at ' + crash.t + '</p>'
-                        + '<p>Severity: ' + (crash.s === 'K' ? 'Fatal crash' : crash.s === 'A' ? 'Suspected Serious Injury' : 'Property damage only'
-                        + '<br><p>Trafficway Ownership: ' + crash.s ==='' ? 'Public road' : 'Other')
-                        + '<br><p>Motor vehicle was driving on: ' + crash.o + (crash.h === null ? '' : ' and the nearest cross-street is ' + crash.h + '</p>')
-                        + '<p>There was ' + (crash.f === 'True' ? 'a bike lane ' : 'no bike lane ') + 'present.</p>'
-                        + '<a href="' + diagramUrl + '" target="_blank"><img src="' + diagramUrl + '" style="display:none" alt="Crash diagram" />Show crash diagram.</a>'
-                        + '</span><br>',
-                        { minWidth: 200 }
-                    )
-
-                // Unused function, but could be used to show crash diagram
-                function showDiagram() {
-                    var diagramElement = document.getElementById("diagram");
-                    diagramElement.style.display = "block"
-                }
-
-                circle.on('popupopen', function () {
-                    filters.style.display = "none";
-                    map.setLatLng(map.getCenter() + [0, 0.0001]);
-                    console.log(map.getCenter())
-                });
-
-                circle.on('popupclose', function () {
-                    filters.style.display = "block";
-                });
-
-                individualPoints.addLayer(circle); // add the circle to the layergroup
+            // Adjust heatmap as zoom changes
+            if ( map.getZoom() >= 17 ) {
+                intensity = 1;
+                heat.setOptions({
+                maxZoom: 17,
                 })
 
-                // If zoomed in all the way, show points instead of a heatmap
-                // Adjust heatmap as zoom changes
-                if ( map.getZoom() >= 17 ) {
-                    intensity = 1;
-                    heat.setOptions({
-                    maxZoom: 17,
-                    })
-
-                    if ( map.getZoom() >= 18) {
-                        intensity = 0;
-                    }
-                } else {
-                    intensity = 4;
-                    heat.setOptions({maxZoom: 17,})
+                if ( map.getZoom() >= 18) {
+                    intensity = 0;
                 }
-
-                // Update the heatlayer
-                heat.setLatLngs(crashesFiltered.map(function(point) {
-                        return [point.x, point.y, intensity];}))
             } else {
-                heat.setLatLngs(crashesFiltered.map(function(point) {
-                        return [point.x, point.y, intensity];}))
+                intensity = 4;
+                heat.setOptions({maxZoom: 17,})
             }
+
+          // Update the heatlayer
+          heat.setLatLngs(crashesFiltered.map(function(point) {
+                  return [point.x, point.y, intensity];}))
+          } else {
+              heat.setLatLngs(crashesFiltered.map(function(point) {
+                      return [point.x, point.y, intensity];}))
+          }
 
         } // End of updateHeatLayer function
 
